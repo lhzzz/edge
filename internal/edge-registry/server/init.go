c0f64767f64d387fde22aa9ff8418041c5baae13
lhzzz <985854294@qq.com>
2022-05-13 17:56:43 +0800

    add service

--- internal/edge-registry/server/init.go
+++ internal/edge-registry/server/init.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"edge/pkg/common"
 	"edge/pkg/kubeclient"
 
 	"edge/internal/constant/manifests"
@@ -11,6 +12,8 @@ const (
 )
 
 func initResource() error {
+	common.InitLogger()
+
 	if err := kubeclient.CreateResourceWithFile(getK8sClient(), manifests.EdgeIngressYaml, nil); err != nil {
 		return err
 	}
