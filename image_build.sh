c338f7f3b4d686e31790f211b2dd265a71353c35
林豪 <18100176883@163.com>
2022-07-01 11:09:44 +0800

    增加多架构镜像构建

--- image_build.sh
+++ image_build.sh
@@ -3,7 +3,7 @@ set -e
 
 CMD=cmd/
 BIN=bin/
-DOCKERFILE=build/docker/
+DOCKERFILE=build/docker
 REGISTRY=registry.zhst.com
 VERSION=v1.
 
@@ -13,22 +13,34 @@ then
     docker login ${REGISTRY}
     for d in $(ls $CMD -l | grep ^d | awk '{print $9}')
     do 
-        if [ "$(ls ${DOCKERFILE}${d})" ]; then 
-            cp ${BIN}${d} ${DOCKERFILE}${d}/
-            docker build -t ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:${VERSION}${CI_PIPELINE_ID} ${DOCKERFILE}${d}/
-            docker push ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:${VERSION}${CI_PIPELINE_ID}
-            rm ${DOCKERFILE}${d}/${d}
-            echo ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:${VERSION}${CI_PIPELINE_ID}
+    {   if [ "$(ls ${DOCKERFILE}/${d})" ]; then 
+            PLATFORM=""
+            if [ "$(ls ${BIN}/amd64/${d})" ]; then 
+                PLATFORM="linux/amd64"
+                cp ${BIN}/amd64/${d} ${DOCKERFILE}/${d}/mutiple/amd64
+            fi
+            if  [ "$(ls ${BIN}/arm64/${d})" ]; then 
+                PLATFORM=$PLATFORM",linux/arm64"
+                cp ${BIN}/arm64/${d} ${DOCKERFILE}/${d}/mutiple/arm64
+            fi
+            docker buildx build --platform ${PLATFORM} -t ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:${VERSION}${CI_PIPELINE_ID} ${DOCKERFILE}/${d}/mutiple/ --push
         fi
+    }&
+    done
+    wait
+    for d in $(ls $CMD -l | grep ^d | awk '{print $9}')
+    do
+        echo ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:${VERSION}${CI_PIPELINE_ID}
     done
 else 
     CI_PROJECT_NAMESPACE=cloud-native
+    GOARCH=$(go env GOARCH)
     for d in $(ls $CMD -l | grep ^d | awk '{print $9}')
     do 
-        if [ "$(ls ${DOCKERFILE}${d})" ]; then 
-            cp ${BIN}${d} ${DOCKERFILE}${d}/
-            docker build -t ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:local ${DOCKERFILE}${d}/
-            rm ${DOCKERFILE}${d}/${d}
+        if [ "$(ls ${DOCKERFILE}/${d})" ]; then 
+            cp ${BIN}${GOARCH}/${d} ${DOCKERFILE}/${d}/single
+            docker build -t ${REGISTRY}/${CI_PROJECT_NAMESPACE}/${d}:local ${DOCKERFILE}/${d}/single/
+            rm ${DOCKERFILE}/${d}/single/${d}
         fi
     done
 fi
